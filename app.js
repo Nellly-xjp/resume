7a743c98c15d8566c7a398573cee5063e9a82401
Nelly <nellytyrkanych@gmail.com>
2026-02-13 00:33:51 +0200

    add three.js

---
+++ app.js
@@ -0,0 +1,10 @@
+VANTA.FOG({
+  el: "#intro",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200,
+  minWidth: 200,
+  midtoneColor: 0xafffb5,
+  lowlightColor: 0x2f2564
+})
