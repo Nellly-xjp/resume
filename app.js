584fbd07b059b2a8ef92e976c1919d5c081d558b
Nelly <nellytyrkanych@gmail.com>
2026-03-13 22:47:08 +0200

    adaptyvnyst

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ VANTA.FOG({
   mouseControls: true,
   touchControls: true,
   gyroControls: false,
+  
   minHeight: 200,
   minWidth: 200,
-  midtoneColor:   0xffb07c,  
-  lowlightColor:  0xff8a5e,  
-  baseColor:      0xff6b4a,  
-  blurFactor: 0.65,
-  speed: 0.8
+  
+  highlightColor: 0xFF7518,   
+  midtoneColor:   0xff8a5e,   
+  lowlightColor:  0x8a3f1e,   
+  baseColor:      0x0f0a08,   
+  
+  blurFactor: 0.45,           
+  speed: 0.4,                 
+  zoom: 1.0,                  
+  
+
+   scale: 5.0,             
+   scaleMobile: 5.0         
 });
